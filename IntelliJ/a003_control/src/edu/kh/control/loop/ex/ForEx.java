106b4b2cc1551e1df9c8ab29cfd45ee0c40b4470
yuna <cyunanne@gmail.com>
2023-02-10 12:49:18 +0900

    23.2.10. 오전 수업

--- IntelliJ/a003_control/src/edu/kh/control/loop/ex/ForEx.java
+++ IntelliJ/a003_control/src/edu/kh/control/loop/ex/ForEx.java
@@ -41,4 +41,97 @@ public class ForEx {
         }
         System.out.println(result);
     }
+
+    // for문 기초 사용법6: 다른 자료형으로 for문 사용
+    public void ex6() {
+        for (double i = 10; i <= 20; i += 0.5) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n-----------------------------------------");
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c);
+        }
+    }
+
+    // for문 응용 사용법1: 감소
+    public void ex7() {
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // for문 응용 사용법2: 정수 5개를 입력받아 합계 출력
+    public void ex8() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i + 1) + "번째 숫자 입력: ");
+            sum += sc.nextInt();
+        }
+        System.out.println(sum);
+    }
+
+    // for문 응용 사용법3: 반복횟수 사용자입력
+    public void ex9() {
+        Scanner sc = new Scanner(System.in);
+        int fin, sum = 0;
+
+        System.out.print("입력 받을 정수의 갯수: ");
+        fin = sc.nextInt();
+
+        for (int i = 0; i < fin; i++) {
+            System.out.print((i + 1) + "번째 숫자 입력: ");
+            sum += sc.nextInt();
+        }
+
+        System.out.println(sum);
+    }
+
+    // for문 응용 사용법5: 1~10출력, 3배수:[n], 5배수:@
+    public void ex11() {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 3 == 0) {
+                System.out.printf("[%d] ", i);
+            } else if (i % 5 == 0) {
+                System.out.print("@ ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // for문 응용 사용법6: 구구단 2단
+    public void ex12() {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("2 x %d = %2d\n", i, 2 * i);
+        }
+    }
+
+    // for문 응용 사용법7: 구구단 입력, 역순 출력
+    public void ex13() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("구구단 : ");
+        int input = sc.nextInt();
+        for (int i = 9; i >= 1; i--) {
+            System.out.printf("%d × %d = %2d\n", input, i, input * i);
+        }
+    }
+
+    // for문 응용 사용법8: 구구단 출력
+    public void ex14() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("구구단 : ");
+        int input = sc.nextInt();
+
+        if (input < 2 || input > 9) {
+            System.out.println("잘못 입력하셨습니다.");
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                System.out.printf("%d × %d = %2d\n", input, i, input * i);
+            }
+        }
+    }
 }
--- IntelliJ/a003_control/src/edu/kh/control/loop/run/ForRun.java
+++ IntelliJ/a003_control/src/edu/kh/control/loop/run/ForRun.java
@@ -11,6 +11,14 @@ public class ForRun {
 //        fe.ex2();
 //        fe.ex3();
 //        fe.ex4();
-        fe.ex5();
+//        fe.ex5();
+//        fe.ex6();
+//        fe.ex7();
+//        fe.ex8();
+//        fe.ex9();
+//        fe.ex11();
+//        fe.ex12();
+//        fe.ex13();
+        fe.ex14();
     }
 }
