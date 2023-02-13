b3ce6f9c9a2e8767b620108243740cf0f09dfcca
DESKTOP-IJ8C807\user1 <cyunanne@gmail.com>
2023-02-13 09:45:05 +0900

    eclipse intellij 동기화

--- eclipse/a003_control/src/edu/kh/control/condition/practice/ConditionPracticeRun.java
+++ eclipse/a003_control/src/edu/kh/control/condition/practice/ConditionPracticeRun.java
@@ -1,5 +1,7 @@
 package edu.kh.control.condition.practice;
 
+import edu.kh.control.condition.practice.ConditionPractice;
+
 public class ConditionPracticeRun {
 	
 	public static void main(String[] args) {
@@ -9,7 +11,8 @@ public class ConditionPracticeRun {
 //		cp.practice1();
 //		cp.practice2();
 //		cp.practice3();
-		cp.practice4();
+//		cp.practice4();
+		cp.practice5();
 	}
 
 }
---
+++ eclipse/a003_control/src/edu/kh/control/loop/run/ForRun.java
@@ -0,0 +1,12 @@
+package edu.kh.control.loop.run;
+
+import edu.kh.control.loop.ex.ForEx;
+
+public class ForRun {
+
+    public static void main(String[] args) {
+
+        ForEx fe = new ForEx();
+        fe.ex22();
+    }
+}
