6132d886cce74553e20684e8b77ea9b839c71f3c
yuna <cyunanne@gmail.com>
2023-04-13 21:54:48 +0900

    오늘 오후수업

--- KHStudy/04_WebFront/03_JavaScript/js/08_윈도우내장객체.js
+++ KHStudy/04_WebFront/03_JavaScript/js/08_윈도우내장객체.js
@@ -48,3 +48,32 @@ let time = window.setInterval(currentTime, 1000);
 const stop = document.getElementById("stop").addEventListener("click", () => {
     clearInterval(time);
 });
+
+// 팝업창 열기
+const openPopup1 = document.getElementById('openPopup1');
+const openPopup2 = document.getElementById('openPopup2');
+const openPopup3 = document.getElementById('openPopup3');
+
+openPopup1.addEventListener("click", () => {
+    // widnow.open(url);
+    // js 경로 X -> 브라우저에 보이는 주소
+    window.open("07_함수.html"); // 새 탭에서 열기
+});
+
+openPopup2.addEventListener("click", () => {
+    // 새 창에서 최소한의 팝업창 요건을 가진 상태로 열기
+    // window.open("url", "_blank", "popup");
+    window.open("07_함수.html", "_blank", "popup");
+});
+
+openPopup3.addEventListener("click", () => {
+    // 새 창의 크기를 너비 400px, 높이 600px로 지정
+    // 열리는 위치는 위에서 100px, 왼쪽에서 100px 떨어진 위치
+    // window.open("url", "_blank", "popup");
+    window.open("07_함수.html", "_blank", "width=400, height=600, top=100, left=100"); // 옵션 기본단위 : px
+});
+
+// 팝업창으로 값 전달
+document.getElementById("sendPopup").addEventListener("click", () => {
+    window.open("09_팝업.html", "popupWindow", "width=500, height=300");
+});
